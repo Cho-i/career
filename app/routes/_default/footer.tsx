2e0a30aa474cabf2d66a25c5cb700aadf58a4cea
Cho-i <cho.i.lee.1223@gmail.com>
2025-05-20 20:40:29 +0900

    수정

--- app/routes/_default/footer.tsx
+++ app/routes/_default/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="py-6 text-center bg-neutral-200">
-      <p className="text-xs text-black">cho.i.lee.1223@gmail.com</p>
+      <p className="text-xs text-black">https://github.com/Cho-i</p>
     </footer>
   );
 }
